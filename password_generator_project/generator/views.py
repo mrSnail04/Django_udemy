de01dbce30784e9bcbf44f6b712a5bbc13a7c4ef
Maksim Bulauski <maks.bulavsky@gmail.com>
2021-06-06 15:10:44 +0300

    .

--- password_generator_project/generator/views.py
+++ password_generator_project/generator/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
+
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'generator/home.html', {'password':'sdfs54sdf46sf'})
+    return render(request, 'generator/home.html')
+
+
+def password(request):
+
+    charactrs = list('qwertyuiopasdfghjklzxcvbnm')
+    length = 10
+    thepassword = ''
+
+    for letters in range(length):
+        thepassword += random.choice(charactrs)
+
+
+    return render(request, 'generator/password.html', {'password': thepassword})
