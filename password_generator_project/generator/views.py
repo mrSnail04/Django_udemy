369c5cd93ed5a1776d4b4ab5da7a6c8a39f721e5
Maksim Bulauski <maks.bulavsky@gmail.com>
2021-06-15 19:48:42 +0300

    update password-generator. Connect bootstrap. add new page: -about

--- password_generator_project/generator/views.py
+++ password_generator_project/generator/views.py
@@ -9,10 +9,26 @@ def home(request):
     return render(request, 'generator/home.html')
 
 
+def about(request):
+    return render(request, 'generator/about.html')
+
+
 def password(request):
 
     charactrs = list('qwertyuiopasdfghjklzxcvbnm')
-    length = 10
+
+    if request.GET.get('uppercase'):
+        charactrs.extend('QWERTYUIOPASDFGHJKLZXCVBNM')
+    if request.GET.get('numbers'):
+        charactrs.extend('1234567890')
+    if request.GET.get('special'):
+        charactrs.extend('!@#$%^&*()_+=')
+
+    length = int(request.GET.get('length', 8))
+
+    if length > 30:
+        raise Exception('Слишком большая длина пароля')
+
     thepassword = ''
 
     for letters in range(length):
